f2eb7e9744b8eb8eaa8243d83da9cfdad161aec1
Alessandro Caprarelli <alessandro.caprarelli@truelayer.com>
2024-07-06 00:05:43 +0200

    feat: add logger config

---
+++ backend/config/logger.ts
@@ -0,0 +1,17 @@
+
+'use strict';
+
+const { format } = require('logform');
+
+const alignedWithColorsAndTime = format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.printf((info) => {
+        const { level, message, timestamp, ...meta } = info;
+        return `${timestamp} ${level}: ${message}. ${JSON.stringify(meta)}`
+    })
+);
+
+export default {
+    format: alignedWithColorsAndTime,
+};
