b1ec8dbd2308a5747baf979a7c1b717e03d8ff53
Alessandro Caprarelli <alessandro.caprarelli@truelayer.com>
2024-07-10 09:39:16 +0200

    feat: added login page for user before the order one

--- frontend/layouts/admin.vue
+++ frontend/layouts/admin.vue
@@ -2,6 +2,18 @@
   <div class="preloader" v-if="loading">
     🍅🥕🍆🍆🥜🥜🐝🌻
   </div>
+  <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Local Gems</a>
+
+    <ul class="navbar-nav flex-row d-md-none">
+      <li class="nav-item text-nowrap">
+        <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu"
+          aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="bi-list"></i>
+        </button>
+      </li>
+    </ul>
+  </header>
   <div class="container-fluid">
     <div class="row">
       <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
