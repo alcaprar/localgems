2797766271d94560c5203e285e9a4906728f47cd
Alessandro Caprarelli <alessandro.caprarelli@truelayer.com>
2024-07-11 17:44:50 +0200

    fix: open sale details when clicking on the row and moved `+` button next to the title

--- frontend/pages/shop/sales/index.vue
+++ frontend/pages/shop/sales/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Finestre di vendita</h1>
+    <h1 class="h2">Finestre di vendita <NuxtLink :to="`/shop/sales/new`"> <button type="button"
+          class="btn btn-primary"><i class="bi-plus-circle-fill" /></button></NuxtLink>
+    </h1>
   </div>
   <div class="table-responsive small">
     <table class="table table-striped table-sm">
@@ -10,21 +12,15 @@
           <th>Data inizio</th>
           <th>Data fine</th>
           <th>Stato</th>
-          <th>
-            <NuxtLink :to="`/shop/sales/new`"><i class="bi-plus-circle-fill" /></NuxtLink>
-          </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="sale in sales" :key="sale.id">
+        <tr v-for="sale in sales" :key="sale.id" @click="rowClicked(sale.id)">
           <td>{{ sale.id }}</td>
           <td>{{ sale.startDate.toLocaleString() }}</td>
           <td>{{ sale.endDate.toLocaleString() }}</td>
           <td>
             {{ isSaleActive(sale) ? "🟢 In corso" : "🔴 Conclusa" }}
-          </td>
-          <td>
-            <NuxtLink :to="`/shop/sales/${sale.id}`"><i class="bi-pencil-fill" /></NuxtLink>
           </td>
         </tr>
       </tbody>
@@ -50,6 +46,9 @@ export default {
     this.$loader.stopLoader();
   },
   methods: {
+    rowClicked(saleId: string) {
+      navigateTo(`/shop/sales/${saleId}`)
+    },
     isSaleActive(sale: Sale): boolean {
       const now = new Date();
       return now >= sale.startDate && now <= sale.endDate
